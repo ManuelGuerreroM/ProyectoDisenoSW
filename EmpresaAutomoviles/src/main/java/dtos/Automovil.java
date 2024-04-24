1818f868476cdafe4b35516f41aa14047e9e1123
DANNYS_PC\daniel <danielcastro.f50@gmail.com>
2024-04-24 02:30:39 -0700

    Creación de la Codificación del Proyecto del Caso de uso en Netbeans con Maven. Creación de los paquetes dto, recursosMultimedia, principal, excepciones y guis. El paquete de dtos es para obtener los datos de los objetos, el de recursosMultimedia para imágenes y diseño de las GUI, principal para obtener la clase principal de todo el proyecto en caso de ser necesario o ser utilizada. excepciones todavía no tiene implementación, pero después lo tendrá y el paquete Guis contiene las interfaces gráficas con las que el usuario va a interactuar. De las GUI creé la de Iniciar sesión con un diseño básico; los botones aún no tienen funcionalidad, pero lo tendrán.

---
+++ EmpresaAutomoviles/src/main/java/dtos/Automovil.java
@@ -0,0 +1,16 @@
+/**
+ * Automovil.java
+ * 
+ * Esta clase fue creada el 24 de Abril del 2024, a las 12:36 A.M.
+ */
+package dtos;
+
+/**
+ * El propósito de esta clase es...
+ * 
+ * @authors: Daniel Alejandro Castro Félix - 235294
+ *           Manuel 
+ */
+public class Automovil {
+    
+}
---
+++ EmpresaAutomoviles/src/main/java/dtos/Personal.java
@@ -0,0 +1,16 @@
+/**
+ * Personal.java
+ * 
+ * Esta clase fue creada el 24 de Abril del 2024, a las 12:38 A.M.
+ */
+package dtos;
+
+/**
+ * El propósito de esta clase es...
+ * 
+ * @authors: Daniel Alejandro Castro Félix - 235294
+ *           Manuel 
+ */
+public class Personal {
+    
+}
